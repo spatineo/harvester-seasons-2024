b829c14467940d3fa59e48b22528dae68d8ff84e
Stanley Festus <stanley.festus@spatineo.com>
2023-04-05 05:59:29 +0000

    Removed unused variables

--- src/MainView/MainViewComponent.tsx
+++ src/MainView/MainViewComponent.tsx
@@ -1,72 +1,124 @@
 import { useState, useEffect } from 'react'
 import { Container, Box } from '@mui/material'
+import GeoJSON from 'ol/format/GeoJSON';
 import * as olSource from "ol/source"
 import { useAppSelector } from '../store/hooks'
 import MapComponent from '../MapComponent/MapComponent'
 import HeadingComponent from '../HeadingCompnent/HeadingComponent'
-import BaseControl from '../Controls/BaseControl'
 import GraphComponent from '../GraphComponent/GraphComponent'
 import Layer from '../Layers/Layers'
-import TileLayer from '../Layers/TileLayers'
+import Maastokartta from '../Layers/Maastokartta'
+import Thunderforest from '../Layers/Thunderforest'
+import Taustakartta from '../Layers/Taustakartta'
+import TileLayer from '../Layers/TileLayer'
+import XYZ from 'ol/source/XYZ'
+import VectorSource from 'ol/source/Vector';
 import axios from 'axios'
 import '../Map.css'
 
 function MainViewComponent() {
   const [graphData, setGraphData] = useState<[]>([])
+  const [graphDataTwo, setGraphDataTwo] = useState<[]>([])
+  const [graphDataThree, setGraphDataThree] = useState<[]>([])
   const [summer, setSummer] = useState<string>('')
+  const [yValueForGraphTwo, setYValueForGraphTwo] = useState('')
   const mapState = useAppSelector(state => state.mapState)
-
-  const SHensemble = "SD-M:ECBSF::1:0:1:0";
-  const param1="utctime";
-  const param2="HARVIDX{0.4;VSW-M3M3:ECBSF:5022:9:7:0:1-50;VSW-M3M3:ECBSF:5022:9:7:0:0}";
-  const param3="HARVIDX{273;TSOIL-K:ECBSF:::7:3:1-50;TSOIL-K:ECBSF:::7:1:0}";
-  const param4="ensover{0.4;0.9;SD-M:ECBSF::1:0:3:1-50;SD-M:ECBSF::1:0:1:0}";
-  const SWensemble2 = "DIFF{VSW-M3M3:ECBSF:5022:9:7:0:0;SWVL2-M3M3:SMARTMET:5015}";
-  let SHensemble2 = "DIFF{SD-M:ECBSF::1:0:1:0;HSNOW-M:SMARTOBS:13:4}";
-  const param5 = "HARVIDX{0.4;SWVL2-M3M3:SMARTMET:5015}";
-  const param6 = "HARVIDX{-0.7;STL1-K:SMARTMET}";
-  const param7 = "ensover{0.4;0.9;SD-M:SMARTMET:5027}";
-  const param8 = "ensover{0.4;0.9;HSNOW-M:SMARTOBS:13:4}";
-  const latlonPoint = "64.22728,27.72846"
-  const now = new Date();
-
+  
   let lon = mapState.position.lon
   let lat = mapState.position.lat
 
     // latlon=64,27
   useEffect(() => {
-    try {
-      if(lon > 0 && lat > 0){
-        const url = `https://sm.harvesterseasons.com/timeseries?latlon=${lat},${lon}&param=utctime,VSW-M3M3:ECBSF:5022:9:7:0:0,VSW-M3M3:ECBSF:5022:9:7:0:1,
-        VSW-M3M3:ECBSF:5022:9:7:0:2,VSW-M3M3:ECBSF:5022:9:7:0:3,VSW-M3M3:ECBSF:5022:9:7:0:4,DIFF{VSW-M3M3:ECBSF:5022:9:7:0:0;-0.0305226445198059},
-        DIFF{VSW-M3M3:ECBSF:5022:9:7:0:1;-0.0305470526218414},
-        DIFF{VSW-M3M3:ECBSF:5022:9:7:0:2;-0.0306599736213684},DIFF{VSW-M3M3:ECBSF:5022:9:7:0:3;-0.007231593132019},DIFF{VSW-M3M3:ECBSF:5022:9:7:0:4;-0.0304692387580872},
-        SWVL2-M3M3:SMARTMET:5015&starttime=202303020000&endtime=202310040000&timestep=1440&timeformat=sql&precision=full&separator=,&source=grid&tz=utc&format=json`
-  
-        axios.get(url).then((response) => {
-          if(response.status === 200){
-            const g = response.data
-            setGraphData(g)
-          }
-        })
-        .catch((error) => {
-          console.log(error.message, 'axios error')
-        })
+  let url: string;
+  const graphData = async () => {  
+    if(lon > 0 && lat > 0){
+    url = `https://sm.harvesterseasons.com/timeseries?latlon=${lat},${lon}&param=utctime,VSW-M3M3:ECBSF:5022:9:7:0:0,VSW-M3M3:ECBSF:5022:9:7:0:1,
+    VSW-M3M3:ECBSF:5022:9:7:0:2,VSW-M3M3:ECBSF:5022:9:7:0:3,VSW-M3M3:ECBSF:5022:9:7:0:4,DIFF{VSW-M3M3:ECBSF:5022:9:7:0:0;-0.0305226445198059},
+    DIFF{VSW-M3M3:ECBSF:5022:9:7:0:1;-0.0305470526218414},
+    DIFF{VSW-M3M3:ECBSF:5022:9:7:0:2;-0.0306599736213684},DIFF{VSW-M3M3:ECBSF:5022:9:7:0:3;-0.007231593132019},DIFF{VSW-M3M3:ECBSF:5022:9:7:0:4;-0.0304692387580872},
+    SWVL2-M3M3:SMARTMET:5015&starttime=202303020000&endtime=202310040000&timestep=1440&timeformat=sql&precision=full&separator=,&source=grid&tz=utc&format=json`
+    }
+
+    try{
+      if(url){
+        await axios
+        .get(url)
+        .then((response) => {
+          if(response.status === 200)
+          setGraphData(response.data)
+        }).catch((err) => console.log(err));
       }
-     
-   
-  } catch (error) {
-    console.error( error)
+    } catch (error){
+      console.log('try and catch error')
+    }
   }
+
+  const graphDataTwo = async () => {
+    if(lon > 0 && lat > 0){
+      url = `https://sm.harvesterseasons.com/timeseries?latlon=${lat},${lon}&param=utctime,DIFF{SD-M:ECBSF::1:0:1:0;HSNOW-M:SMARTOBS:13:4}
+    ,DIFF{SD-M:ECBSF::1:0:3:1;HSNOW-M:SMARTOBS:13:4},DIFF{SD-M:ECBSF::1:0:3:2;HSNOW-M:SMARTOBS:13:4},DIFF{SD-M:ECBSF::1:0:3:3;HSNOW-M:SMARTOBS:13:4},
+    DIFF{SD-M:ECBSF::1:0:3:4;HSNOW-M:SMARTOBS:13:4}
+    &starttime=20230310T000000Z&endtime=202303200000&timestep=1440&format=json&precision=full&tz=utc&timeformat=xml
+    `}
+    try{
+      await axios
+      .get(url)
+      .then((response) => setGraphDataTwo(response.data))
+      .catch((err) => console.log(err));
+    } catch (error){
+      console.log('try and catch error', error)
+    }
+  }
+  graphData()
+  graphDataTwo()
+
+  console.log(import.meta.env.VITE_API_KEY) 
+
+
   },[lon, lat])
  
-  const option = {
+  const optionDataOne = {
     color: ['green', 'blue' ],
     dataset: {
     source: [
       ["type",...graphData.map((d: any) => d.utctime.split(' ')[0])],
-      [`${summer}` ? `${summer}` : 'Summer Index', ...graphData.map((data: any) => data['DIFF{VSW-M3M3:ECBSF:5022:9:7:0:0;-0.0305226445198059}'])],
+      [`${summer}` ? `${summer}` : 'Summer Index', ...graphData.map((data: any) => data['DIFF{VSW-M3M3:ECBSF:5022:9:7:0:0;-0.0305226445198059}'] )],
       ["Winter Index", ...graphData.map((data: any) => data['VSW-M3M3:ECBSF:5022:9:7:0:0'])],
+    ]
+  },
+  legend: {},
+ 
+  tooltip: {
+    trigger: 'axis'
+  },
+  yAxis: {
+    name: 'Traficability'
+  },
+  xAxis: {
+    type: "category",
+    axisTick: {
+      show: false
+    }
+  },
+  series: [{
+    type: "line",
+    seriesLayoutBy: "row",
+  },
+  {
+    type: "line",
+    seriesLayoutBy: "row"
+  }]
+  }
+
+  const optionDataTwo = {
+    color: ['green', 'blue' ],
+    dataset: {
+    source: graphDataTwo && [
+      ["type",...graphDataTwo.map((d: any) => d.utctime.split('T')[0])],
+      [`${yValueForGraphTwo}` && `${yValueForGraphTwo}` , ...graphDataTwo.map((data: any) => {
+        data['DIFF{SD-M:ECBSF::1:0:1:0;HSNOW-M:SMARTOBS:13:4}']
+      })],
+      [`${yValueForGraphTwo}` && `${yValueForGraphTwo}`, ...graphDataTwo.map((data: any) => data['DIFF{SD-M:ECBSF::1:0:1:0;HSNOW-M:SMARTOBS:13:4}'])],
     ]
   },
   legend: {},
@@ -110,7 +162,7 @@ function MainViewComponent() {
         <Box>
         {graphData.length > 0 ?
           <GraphComponent 
-            option={option} 
+            option={optionDataOne} 
             onEvents={{
               'mouseover': onMouseover,
               'globalout': onGlobalout,
@@ -121,16 +173,42 @@ function MainViewComponent() {
           }
         </Box>
         <MapComponent resolution={mapState.position.resolution} center={[mapState.position.lon, mapState.position.lat]}>
-          {/* <BaseControl /> */}
           <Layer>
-            <TileLayer zIndex={0} sources={{
-              source: new olSource.OSM(),
-              title: 'base',
-              name: 'base',
-            }}  />
+          <Taustakartta source={new VectorSource({
+              attributions: ' <a href="https://www.thunderforest.com/">Thunderforest</a> Data by <a href="https://www.fmi.fi/">Finnish Meteorological Institute</a>',
+              url: 'https://openlayers.org/data/vector/ecoregions.json',
+              format: new GeoJSON()})} />
+            <Maastokartta
+              source={
+                new olSource.OSM({
+                  url: 'https://avoin-karttakuva.maanmittauslaitos.fi/avoin/wmts/1.0.0/maastokartta/default/WGS84_Pseudo-Mercator/{z}/{y}/{x}.png?api-key=45deef08-fd2f-42ae-9953-5550fff43b17',
+                  attributions: '<a href="https://www.thunderforest.com/">Thunderforest</a> Data by <a href="https://www.fmi.fi/">Finnish Meteorological Institute</a>',
+                  })}
+                    />
+            <TileLayer source={new olSource.OSM(({
+              url: 'https://avoin-karttakuva.maanmittauslaitos.fi/avoin/wmts/1.0.0/taustakartta/default/WGS84_Pseudo-Mercator/{z}/{y}/{x}.png?api-key=45deef08-fd2f-42ae-9953-5550fff43b17',
+              attributions: '<a href="https://www.thunderforest.com/">Thunderforest</a> Data by <a href="https://www.fmi.fi/">Finnish Meteorological Institute</a>',
+            }))} />
+            <Thunderforest source={new XYZ({
+              url: 'https://{a-c}.tile.opentopomap.org/{z}/{x}/{y}.png',
+              attributions: '<a href="https://www.thunderforest.com/">Thunderforest</a> Data by <a href="https://www.fmi.fi/">Finnish Meteorological Institute</a>',
+            })} />
+           
           </Layer>
         </MapComponent>
-        <GraphComponent option={{}} onEvents={{}}  />
+        <Box sx={{marginTop: '2rem'}}>
+        {graphDataTwo.length > 0 ?
+          <GraphComponent 
+            option={optionDataTwo} 
+            onEvents={{
+              'mouseover': (event: any) => setYValueForGraphTwo(event.data[1]),
+              'globalout': (event: any) => setYValueForGraphTwo(''),
+            }} 
+          /> 
+         :
+          <Box sx={{textAlign: 'center', marginTop: '4rem'}}>...Loading graph data</Box>
+          }
+        </Box>
       </Container>
     </div>
   )
