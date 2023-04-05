041352b7011d4263f2aedfa8768894e1b699bf5f
Stanley Festus <stanley.festus@spatineo.com>
2023-04-05 05:59:45 +0000

    styles edited

--- src/MapComponent/MapComponent.tsx
+++ src/MapComponent/MapComponent.tsx
@@ -5,17 +5,19 @@ import { setPosition } from './MapComponentSlice';
 import MapContext from './MapContext';
 import * as ol from "ol"
 import { defaults as defaultControls } from "ol/control";
+import LayerSwitcher from 'ol-layerswitcher';
 import proj4 from 'proj4'
-import 'ol/ol.css';
 
 const styles = {
   mapTextContainer: {
     display: 'flex', 
-    flexDirection: 'row', 
-    margin: '0.6rem 0rem', 
+    flexDirection: 'row',
+    margin: 'auto',
     paddingLeft: '0px',
+    width: '88%',
+    border: '1px solid black'
   },
-  innerTextStyle: {
+  innerBoxStyle: {
     display: 'flex', 
     flexDirection: 'row', 
     justifyContent: 'space-between',
@@ -28,7 +30,7 @@ const styles = {
     verticalAlign: 'bottom'
   },
   typography: {
-    fontSize: '0.87rem',
+    fontSize: '0.8rem',
     padding: '0.1rem'
   }
 }
@@ -56,16 +58,17 @@ const MapComponent: React.FC<MapProps> = ({
   const mapState = useAppSelector(state => state.mapState)
   
   useEffect(() => {
+      
     let options = {
       view: new ol.View({ resolution, center }),
       layers: [],
-      controls: defaultControls(),
+      controls: defaultControls().extend([new LayerSwitcher()]),
       overlays: []
     };
+
     let mapObject: any = new ol.Map(options);
-   
+    
     mapObject.setTarget(mapRef.current);
-  
     setMap(mapObject);
     return () => mapObject.setTarget(undefined);
   }, [mapRef]);
@@ -75,7 +78,7 @@ const MapComponent: React.FC<MapProps> = ({
       navigator.geolocation.getCurrentPosition((position) => {
         const lon = position.coords.longitude
         const lat = position.coords.latitude
-        console.log('how many times geolocation runs')
+       
         dispatch(setPosition({lat, lon}))
         },(error) => {
           console.log(error.code, 'error from geolocation', error.message)
@@ -100,27 +103,27 @@ const MapComponent: React.FC<MapProps> = ({
         {children}
       </Box>
       <Box sx={styles.mapTextContainer}>
-        <Box sx={styles.innerTextStyle}>
+        <Box sx={styles.innerBoxStyle}>
           <Box sx={styles.box} component="span" style={{ background: 'rgb(83, 127, 70)', marginLeft: '0.3rem' }}></Box>
           <Typography component="span" sx={styles.typography}>Frost heave (kelirikko, GOOD)</Typography>
         </Box>
-        <Box sx={styles.innerTextStyle}>
+        <Box sx={styles.innerBoxStyle}>
           <Box style={{background: 'rgb(139, 172, 82)', marginLeft: '0.3rem'}}  component="span" sx={styles.box}></Box>
           <Typography component="span" sx={styles.typography}>Normal summer, mineral soil</Typography>
         </Box>
-        <Box sx={styles.innerTextStyle}>
+        <Box sx={styles.innerBoxStyle}>
           <Box sx={styles.box} component="span" style={{ background: 'rgb(189, 225, 105)', marginLeft: '0.3rem' }}></Box>
           <Typography component="span" sx={styles.typography}>Dry summer, mineral soil</Typography>
         </Box>
-        <Box sx={styles.innerTextStyle}>
+        <Box sx={styles.innerBoxStyle}>
           <Box sx={styles.box} component="span" style={{ background: 'rgb(254, 251, 117)', marginLeft: '0.3rem' }}></Box>
           <Typography component="span" sx={styles.typography}>Normal summer, peat soil</Typography>
         </Box>
-        <Box sx={styles.innerTextStyle}>
+        <Box sx={styles.innerBoxStyle}>
           <Box sx={styles.box} component="span" style={{ background: 'rgb(242, 162, 92)', marginLeft: '0.3rem'}}></Box>
           <Typography component="span" sx={styles.typography}>Dry summer, peat soil</Typography>
         </Box>
-        <Box sx={styles.innerTextStyle}>
+        <Box sx={styles.innerBoxStyle}>
           <Box sx={styles.box} component="span" style={{ background: 'rgb(239, 103, 81)', marginLeft: '0.3rem'}}></Box>
           <Typography component="span" sx={styles.typography}>Winter (BAD)</Typography>
         </Box>
