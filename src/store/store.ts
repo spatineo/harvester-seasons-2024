5ad5a4c0023b4b27df9a1ade2f555f0f94f312de
Stanley Festus <stanley.festus@spatineo.com>
2023-04-28 11:50:46 +0000

    added store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,36 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit"
 import languageSlice from '../Lang/languageSlice'
-import MapComponentSlice from "../MapComponent/MapComponentSlice";
+import {all, fork, takeLatest} from 'redux-saga/effects'
+import { SagaIterator } from 'redux-saga'
+import createSagaMiddleware from 'redux-saga'
+import MapComponentSlice from "../MapComponent/MapComponentSlice"
+import globalSlice, { actions } from "../globalSlice"
+import { useAppSelector } from "./hooks"
+import { watchHarvesterRequests } from "./saga/saga"
 
+function* test() {
+  yield takeLatest('global/setTrafficabilityData', function* ({ payload }: ReturnType<typeof actions.setTrafficabilityData>): SagaIterator {
+
+  })
+}
+export function* rootSaga(): SagaIterator {
+  yield all([fork(watchHarvesterRequests)])
+}
+
+
+const sagaMiddleware = createSagaMiddleware()
 export const store = configureStore({
   reducer: {
     language: languageSlice,
-    mapState: MapComponentSlice
-  }
+    mapState: MapComponentSlice,
+    global: globalSlice
+  },
+  middleware: [sagaMiddleware],
+  devTools: process.env.NODE_ENV !== 'production',
 })
+
+sagaMiddleware.run(rootSaga)
+
 
 export type RootState = ReturnType<typeof store.getState>
 export type RootDispatch = typeof store.dispatch
