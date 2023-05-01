f535803adb799fcffc64f72fe13a3aae75475964
Stanley Festus <stanley.festus@spatineo.com>
2023-05-01 07:03:33 +0000

    working on timeline

--- src/Layers/Thunderforest.tsx
+++ src/Layers/Thunderforest.tsx
@@ -4,7 +4,7 @@ import { BaseLayerOptions } from 'ol-layerswitcher';
 import { useRootDispatch } from "../store/hooks";
 import TileSource from "ol/source/Tile";
 import MapContext from "../MapComponent/MapContext";
-import {Style, Fill} from "ol/style";
+import {Style, Fill, Circle} from "ol/style";
 
 interface OSMLayerProps {
  source: TileSource
@@ -17,12 +17,38 @@ const OSMLayer: React.FC<OSMLayerProps> = ({ source }) => {
  
   useEffect(() => {
     if (!map) return;
+    let cache: any = {};
+    function style(select: any){
+      return function(f: any) {
+        var style = cache[f.get('mag')+'-'+select];
+        if (!style) {
+          var img = new Circle({
+            radius: f.get('mag')*f.get('mag')/2,
+            fill: new Fill({
+              color: select ? 'rgba(255,0,0,.5)':'rgba(255,128,0,.3)'
+            })
+          });
+          var img2 = new Circle({
+            radius: f.get('mag')*f.get('mag')/4,
+            fill: new Fill({
+              color: select ? 'rgba(255,0,0,.5)':'rgba(255,128,0,.3)'
+            })
+          });
+          style = cache[f.get('mag')+'-'+select] = [
+            new Style({ image: img }),
+            new Style({ image: img2 })
+          ];
+        }
+        return style;
+      }
+    };
     
     const osmLayer = new LayerTile({
       background: '#1a2b39',
       title: 'Thunderforest',
       type: 'base',
       source,
+      style: style('blue')
     } as BaseLayerOptions);
 
     map.addLayer(osmLayer)
