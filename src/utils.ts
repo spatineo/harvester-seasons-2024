f5cf253cb7277343507e056bd210de996b068d0c
Stanley Festus <stanley.festus@spatineo.com>
2023-05-04 11:20:08 +0000

    added function to produce code prop values

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { Parameter } from "./types"
+
 export function addTenYears(date: Date, years: number) {
   date.setFullYear(date.getFullYear() + years);
   return date;
@@ -6,4 +8,13 @@ export function addTenYears(date: Date, years: number) {
 export function addSixMonths(date: Date, months: number){
   let newDate = new Date(date.setMonth(date.getMonth()+ months));
   return newDate
+}
+
+export function soilTemperatureCode(arr: Parameter[]){
+  for(let i = 1; i <= 50; i++){
+    arr.push({
+      code: `K2C{TSOIL-K:ECBSF:::7:3:${i}}`
+    })
+  }
+  return arr
 }
