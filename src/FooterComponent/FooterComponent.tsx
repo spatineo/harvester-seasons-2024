c745b79cac8823517bd74e06d4e471a6da826407
Stanley Festus <stanley.festus@spatineo.com>
2023-05-05 11:01:03 +0000

    lint settings enforced

--- src/FooterComponent/FooterComponent.tsx
+++ src/FooterComponent/FooterComponent.tsx
@@ -1,57 +1,70 @@
-import React, { FC} from 'react'
-import { Box, Link, Typography, Button } from '@mui/material'
-import linkedin from '../assets/LI-In-Bug.png'
+/* eslint-disable import/default */
+import React, { FC } from 'react';
+import { Box, Link, Typography, Button } from '@mui/material';
+import linkedin from '../assets/LI-In-Bug.png';
 
 const footerStyle = {
-  container : {
-    display: 'flex',
-    flexDirection: 'row', 
-    justifyContent: 'space-between', 
-    marginBottom: '2rem', 
-    fontFamily: 'Lato',
-    borderTop: '1px solid grey',
-    paddingTop: '0.4rem'
-  },
-  image: {
-    width: '1.6rem',
-    height: '1.2rem',
-    marginRight: '0.2rem',
-  },
-  link: {
-    display: 'flex',
-    textDecoration: 'none',
-    flexDirection: 'row',
-    padding: '0.2rem',
-    color: '#000'
-  },
-  text: {
-    fontFamily: 'Lato',
-  }
-}
+	container: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		marginBottom: '2rem',
+		fontFamily: 'Lato',
+		borderTop: '1px solid grey',
+		paddingTop: '0.4rem',
+	},
+	image: {
+		width: '1.6rem',
+		height: '1.2rem',
+		marginRight: '0.2rem',
+	},
+	link: {
+		display: 'flex',
+		textDecoration: 'none',
+		flexDirection: 'row',
+		padding: '0.2rem',
+		color: '#000',
+	},
+	text: {
+		fontFamily: 'Lato',
+	},
+};
 
 const FooterComponent: FC = () => {
-  return (
-    <Box sx={footerStyle.container}>
-      <Box>
-        <Link href="https://harvesterseasons.com/" target="_blank" sx={footerStyle.link}>
-          <Button>
-            <Typography sx={footerStyle.text}>Harvester Seasons</Typography>
-          </Button>
-        </Link>
-      </Box>
-      <Link href="https://www.linkedin.com/showcase/harvester-seasons" target="_blank" sx={footerStyle.link}>
-        <Button>
-          <Box component="img" src={linkedin} sx={footerStyle.image}/>
-          <Typography component={'span'} sx={footerStyle.text}>Follow us on LinkedIn</Typography>
-        </Button>
-      </Link>
-      <Link href="../privacy-policy/" target="_blank" sx={footerStyle.link}> 
-        <Button>
-          <Typography sx={footerStyle.text}>Privacy Policy / Terms <span>of Use</span></Typography>
-        </Button>
-        </Link>
-    </Box>
-  )
-}
+	return (
+		<Box sx={footerStyle.container}>
+			<Box>
+				<Link
+					href="https://harvesterseasons.com/"
+					target="_blank"
+					sx={footerStyle.link}
+				>
+					<Button>
+						<Typography sx={footerStyle.text}>Harvester Seasons</Typography>
+					</Button>
+				</Link>
+			</Box>
+			<Link
+				href="https://www.linkedin.com/showcase/harvester-seasons"
+				target="_blank"
+				sx={footerStyle.link}
+			>
+				<Button>
+					<Box component="img" src={linkedin} sx={footerStyle.image} />
+					<Typography component={'span'} sx={footerStyle.text}>
+						Follow us on LinkedIn
+					</Typography>
+				</Button>
+			</Link>
+			<Link href="../privacy-policy/" target="_blank" sx={footerStyle.link}>
+				<Button>
+					<Typography sx={footerStyle.text}>
+						Privacy Policy / Terms <span>of Use</span>
+					</Typography>
+				</Button>
+			</Link>
+		</Box>
+	);
+};
 
-export default FooterComponent
+export default FooterComponent;
