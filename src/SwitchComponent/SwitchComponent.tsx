e1936aa29e98d1d756a7e182157c9905f3dd0a81
Stanley Festus <stanley.festus@spatineo.com>
2023-05-05 11:30:05 +0000

    linting

--- src/SwitchComponent/SwitchComponent.tsx
+++ src/SwitchComponent/SwitchComponent.tsx
@@ -1,26 +1,29 @@
-import React, { FC, useState, useEffect } from 'react'
-import { Switch, Box, Typography } from '@mui/material'
+/* eslint-disable import/default */
+import React, { FC } from 'react';
+import { Switch, Box, Typography } from '@mui/material';
 import { useAppSelector, useRootDispatch } from '../store/hooks';
 import { RootState } from '../store/store';
 import { actions } from '../globalSlice';
 
 const SwitchComponent: FC = () => {
-  const dispatch = useRootDispatch()
-  const checked = useAppSelector((state: RootState) => state.global.checked) 
-  return (
-    <Box>
-      <Box sx={{margin: '1rem 0rem '}}>
-        <Switch
-          checked={checked}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-            dispatch(actions.setCheckedButton(event.target.checked))
-          }}
-          sx={{margin: '0rem'}}
-        /> 
-        <Typography sx={{fontFamily: 'Lato'}} component={'span'} >Turn on to change timestep between 6 months and 10 years </Typography>
-      </Box>
-    </Box>
-  )
-}
+	const dispatch = useRootDispatch();
+	const checked = useAppSelector((state: RootState) => state.global.checked);
+	return (
+		<Box>
+			<Box sx={{ margin: '1rem 0rem ' }}>
+				<Switch
+					checked={checked}
+					onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+						dispatch(actions.setCheckedButton(event.target.checked));
+					}}
+					sx={{ margin: '0rem' }}
+				/>
+				<Typography sx={{ fontFamily: 'Lato' }} component={'span'}>
+					Turn on to change timestep between 6 months and 10 years{' '}
+				</Typography>
+			</Box>
+		</Box>
+	);
+};
 
-export default SwitchComponent
+export default SwitchComponent;
