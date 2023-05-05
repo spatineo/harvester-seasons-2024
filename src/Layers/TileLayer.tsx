ced193bd2bab8dff05caa980e3692616344adddc
Stanley Festus <stanley.festus@spatineo.com>
2023-05-05 11:18:16 +0000

    linter cleanup done

--- src/Layers/TileLayer.tsx
+++ src/Layers/TileLayer.tsx
@@ -1,36 +1,39 @@
-import { useEffect, useContext } from "react";
-import MapContext from "../MapComponent/MapContext";
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable react/prop-types */
+import { useEffect, useContext } from 'react';
+import MapContext from '../MapComponent/MapContext';
 import { BaseLayerOptions } from 'ol-layerswitcher';
-import TileSource from "ol/source/Tile";
-import LayerTile from "ol/layer/Tile";
-
+import TileSource from 'ol/source/Tile';
+import LayerTile from 'ol/layer/Tile';
 
 interface TileLayerProps {
-  source: TileSource;
+	source: TileSource;
 }
 
 const TileLayer: React.FC<TileLayerProps> = ({ source }) => {
-  const {map} = useContext(MapContext)
- 
-  useEffect(() => {
-    if (!map) return;
-    
-    const tileLayer = new LayerTile({
-      title: 'TileLayer',
-      type: 'base',
-      visible: true,
-      source,
-      className: 'class'
-    } as BaseLayerOptions)
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+	const { map } = useContext<any>(MapContext);
 
-    map.addLayer(tileLayer)
-    return () => {
-      if (map) {
-        map.removeLayer(tileLayer);
-      }
-    };
-  }, [map]);
-  return null;
+	useEffect(() => {
+		if (!map) return;
+
+		const tileLayer = new LayerTile({
+			title: 'TileLayer',
+			type: 'base',
+			visible: true,
+			source,
+			className: 'class',
+		} as BaseLayerOptions);
+
+		map.addLayer(tileLayer);
+		return () => {
+			if (map) {
+				map.removeLayer(tileLayer);
+			}
+		};
+	}, [map]);
+	return null;
 };
 
 export default TileLayer;
