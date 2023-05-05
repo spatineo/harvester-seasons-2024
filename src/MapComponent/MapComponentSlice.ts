83a3f32d07c27ee4df0110021a3827104f575d40
Stanley Festus <stanley.festus@spatineo.com>
2023-05-05 11:24:01 +0000

    map slice linter indenting applied

--- src/MapComponent/MapComponentSlice.ts
+++ src/MapComponent/MapComponentSlice.ts
@@ -1,31 +1,34 @@
-import { createSlice, PayloadAction} from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 export interface Map {
-  position: { 
-  lat: number;
-  lon: number;
-  resolution: number;
-  },
+	position: {
+		lat: number;
+		lon: number;
+		resolution: number;
+	};
 }
 
 const initialState: Map = {
-  position: {
-    lat: 0,
-    lon: 0,
-    resolution: 3550,
-  }
-}
+	position: {
+		lat: 0,
+		lon: 0,
+		resolution: 3550,
+	},
+};
 
 const mapComponentSlice = createSlice({
-  name: 'map',
-  initialState,
-  reducers: {
-    setPosition: (state, action: PayloadAction<{lat: number, lon: number}>) => {
-      state.position.lat = action.payload.lat
-      state.position.lon = action.payload.lon
-    }
-  }
-})
+	name: 'map',
+	initialState,
+	reducers: {
+		setPosition: (
+			state,
+			action: PayloadAction<{ lat: number; lon: number }>
+		) => {
+			state.position.lat = action.payload.lat;
+			state.position.lon = action.payload.lon;
+		},
+	},
+});
 
-export const { setPosition } = mapComponentSlice.actions
-export default mapComponentSlice.reducer
+export const { setPosition } = mapComponentSlice.actions;
+export default mapComponentSlice.reducer;
