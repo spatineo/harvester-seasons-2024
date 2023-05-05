68a57fc54e63ae34797af1c9ed71065774fd4193
Stanley Festus <stanley.festus@spatineo.com>
2023-05-05 11:09:32 +0000

    eslint update

--- src/HarvesterChartComponent/HarvesterSeasons.tsx
+++ src/HarvesterChartComponent/HarvesterSeasons.tsx
@@ -1,24 +1,24 @@
-import React, { useEffect, useState, FC } from 'react'
-import { Box } from '@mui/material'
-import GraphComponent from '../GraphComponent/GraphComponent'
-import { HarvesterChartProps } from '../types'
+// eslint-disable-next-line import/default
+import React, { FC } from 'react';
+import { Box } from '@mui/material';
+import GraphComponent from '../GraphComponent/GraphComponent';
+import { HarvesterChartProps } from '../types';
 
-const HarvesterSeasons:FC<HarvesterChartProps> = ({
-  option,
-  handleClick
-}) => {
-  return (
-    <Box>
-      {option ? 
-      <GraphComponent 
-        onEvents={{
-        click: handleClick
-       }} 
-       option={option} 
-      /> : <span>No data available</span>
-      }
-    </Box>
-  )
-}
+const HarvesterSeasons: FC<HarvesterChartProps> = ({ option, handleClick }) => {
+	return (
+		<Box>
+			{option ? (
+				<GraphComponent
+					onEvents={{
+						click: handleClick,
+					}}
+					option={option}
+				/>
+			) : (
+				<span>No data available</span>
+			)}
+		</Box>
+	);
+};
 
-export default HarvesterSeasons
+export default HarvesterSeasons;
