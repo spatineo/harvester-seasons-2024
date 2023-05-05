0f5b0e5899a1f10e1dc39d6e08ba9702f32fb5ee
Stanley Festus <stanley.festus@spatineo.com>
2023-05-05 11:27:09 +0000

    linter spacing

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,5 +1,5 @@
-import { TypedUseSelectorHook, useSelector, useDispatch } from "react-redux";
-import { RootDispatch, RootState } from "./store";
+import { TypedUseSelectorHook, useSelector, useDispatch } from 'react-redux';
+import { RootDispatch, RootState } from './store';
 
-export const useRootDispatch = () => useDispatch<RootDispatch>()
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useRootDispatch = () => useDispatch<RootDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
