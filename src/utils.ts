b1e1c379f4c110d1745b7512c4feadf962ad59e4
Stanley Festus <stanley.festus@spatineo.com>
2023-05-05 11:00:32 +0000

    eslint semicolon commas enforced

--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,20 @@
-import { Parameter } from "./types"
+import { Parameter } from './types';
 
 export function addTenYears(date: Date, years: number) {
-  date.setFullYear(date.getFullYear() + years);
-  return date;
+	date.setFullYear(date.getFullYear() + years);
+	return date;
 }
 
-export function addSixMonths(date: Date, months: number){
-  let newDate = new Date(date.setMonth(date.getMonth()+ months));
-  return newDate
+export function addSixMonths(date: Date, months: number) {
+	const newDate = new Date(date.setMonth(date.getMonth() + months));
+	return newDate;
 }
 
-export function soilTemperatureCode(arr: Parameter[]){
-  for(let i = 1; i <= 50; i++){
-    arr.push({
-      code: `K2C{TSOIL-K:ECBSF:::7:3:${i}}`
-    })
-  }
-  return arr
+export function soilTemperatureCode(arr: Parameter[]) {
+	for (let i = 1; i <= 50; i++) {
+		arr.push({
+			code: `K2C{TSOIL-K:ECBSF:::7:3:${i}}`,
+		});
+	}
+	return arr;
 }
