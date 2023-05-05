b4634ea36f1fcd9dd27fd8d5c4cc8b24aba1ad64
Stanley Festus <stanley.festus@spatineo.com>
2023-05-05 10:49:31 +0000

    eslint settings clean up

--- src/Layers/Taustakartta.tsx
+++ src/Layers/Taustakartta.tsx
@@ -1,155 +1,55 @@
-import { useEffect, useContext } from "react";
-import LayerTile from 'ol/layer/Tile';
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { useEffect, useContext } from 'react';
 import { BaseLayerOptions } from 'ol-layerswitcher';
-import { useRootDispatch } from "../store/hooks";
-import Feature from 'ol/Feature'
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
-import MapContext from "../MapComponent/MapContext";
-import Timeline from "ol-ext/control/Timeline";
-import Select from 'ol/interaction/Select'
-import Fill from "ol/style/Fill";
-import BaseEvent from "ol/events/Event";
-import Style from 'ol/style/Style'
-import eCharts from 'echarts'
-import $ from 'jquery'
+import MapContext from '../MapComponent/MapContext';
+import Fill from 'ol/style/Fill';
+import Style from 'ol/style/Style';
+import { Color } from 'ol/color';
 
 interface OSMLayerProps {
- source: VectorSource
+	source: VectorSource;
 }
 
 const style = new Style({
-  fill: new Fill({
-    color: '#eeeeee',
-  }),
+	fill: new Fill({
+		color: '#eeeeee',
+	}),
 });
 
+// eslint-disable-next-line react/prop-types
 const Taustakartta: React.FC<OSMLayerProps> = ({ source }) => {
-  const {map} = useContext(MapContext)
-  const dispatch = useRootDispatch()
- 
-  useEffect(() => {
-    if (!map) return;
-    
-    const vectorLayer = new VectorLayer({
-      title: 'Taustakartta',
-      type: 'base',
-      source, 
-      style: (feature: any) => {
-        const color = feature.get('COLOR') || '#eeeeee';
-        style.getFill().setColor(color);
-        return style;
-      }
-    } as BaseLayerOptions);
-    
-    map.addLayer(vectorLayer)
-   
-    return () => {
-      if (map) {
-        map.removeLayer(vectorLayer);
-      }
-    };
-  }, [map]);
+	const { map } = useContext<any>(MapContext);
 
-  useEffect(() => {
-    if (!map) return;
+	useEffect(() => {
+		if (!map) return;
 
-    const feature: Feature[] = []
-    const minDate_string = new Date('2023-04-01').toDateString()
-    const max_date_string = new Date('2023-10-01').toDateString()
-    const timelineControl = new Timeline({
-      source: source,
-      className: 'ol-pointer',
-      features: feature,
-      // features: [{
-        //text: 2015,
-        //date: new Date('2015/01/01'),
-        // endDate: new Date('2015/12/31')
-     // }],
-      graduation: 'day', // 'month'
-      minDate: minDate_string,
-      maxDate: max_date_string,
-      getHTML: function(f: any){ return 2015; },
-      getFeatureDate: function(f: any){ return f.date; },
-      endFeatureDate: function(f: any) { return f.endDate }
-     
-    });
+		const vectorLayer = new VectorLayer({
+			title: 'Taustakartta',
+			type: 'base',
+			source,
+			style: (feature: any) => {
+				// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+				const color: Color = feature.get('COLOR') || '#eeeeee';
+				style.getFill().setColor(color);
+				return style;
+			},
+		} as BaseLayerOptions);
 
-    map.addControl(timelineControl)
-    let timer = setTimeout(function(){ timelineControl.setDate('2015'); });
-  timelineControl.addButton ({
-    className: "go",
-    title: "GO!",
-    html: 'play',
-    click: (e) => {
-      return ''
-    },
-  });
+		map.addLayer(vectorLayer);
 
-   /*  timelineControl.on('click', function(e: BaseEvent | Event){
-      var d = timelineControl.roundDate(e.date, 'day')
-      $('.dateStart').text(d.toLocaleDateString(undefined, {year: 'numeric', month: 'short', day: 'numeric'}));
-      // Filter features visibility
-      source.getFeatures().forEach(function(f: any) {
-        let dt = f.get('time') - e.target.value;
-        if (Math.abs(dt) > 1000*3600*12) {
-          f.setStyle([]);
-        } else {
-          f.setStyle();
-        }
-      });
-    }); */
+		return () => {
+			if (map) {
+				map.removeLayer(vectorLayer);
+			}
+		};
+	}, [map]);
 
-  let select = new Select({ hitTolerance: 5, });
-  map.addInteraction(select);
-  select.on('select', function(e){
-    var f = e.selected[0];
-    if (f) {
-      // Show info
-			var info = $("#select").html("");
-      $("<p>").text((new Date(f.get("time"))).toLocaleString()).appendTo(info);
-			$("<p>").text('MAG.: '+f.get("mag")+' - '+f.get("place")).appendTo(info);
-		} else {
-      $("#select").html('');
-    }
-  });
-
-  var running = false; 
-  var start = new Date('203-04-01');
-  var end = new Date('2016');
-
-  /* function go(next: any) {
-    let date = timelineControl.getStartDate()
-    if (running) clearTimeout(undefined);
-    if (!next) {
-      // stop
-      if (date>start && date<end && running) {
-        running = false;
-        timelineControl.element.classList.remove('running');
-        return;
-      }
-      if (date > end) {
-        date = start;
-      }
-    }
-    if (date > end) {
-      timelineControl.element.classList.remove('running');
-      return;
-    }
-    if (date < start) {
-      date = start;
-    }
-    // 1 day
-    date = new Date(date.getTime() + 24*60*60*1000);
-    timelineControl.setDate(date, { anim:false });
-    // next
-    timelineControl.element.classList.add('running');
-    running = setTimeout(function() { go(true); }, 100);
-  }
- */
-  
-  }, [map])
-  return null;
+	return null;
 };
 
 export default Taustakartta;
