d66fd9a186684fed0ee7cc26aafe0a4c864f39ea
Stanley Festus <stanley.festus@spatineo.com>
2023-05-05 10:50:58 +0000

    eslint settings applied to app jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import React, { useState, useEffect } from 'react'
-import { Box } from '@mui/material'
-import MainViewComponent from './MainView/MainViewComponent'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, import/default
+import React from 'react';
+import { Box } from '@mui/material';
+import MainViewComponent from './MainView/MainViewComponent';
 import 'ol/ol.css';
 import 'ol-layerswitcher/dist/ol-layerswitcher.css';
-import './App.css'
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Box>
-        <MainViewComponent />
-      </Box>
-    </div>
-  )
+	return (
+		<div className="App">
+			<Box>
+				<MainViewComponent />
+			</Box>
+		</div>
+	);
 }
 
-export default App
+export default App;
