c960516462dec6b100bab9dbd974f5f30d7a3d0b
Stanley Festus <stanley.festus@spatineo.com>
2023-05-05 10:58:35 +0000

    eslint indenting applied

--- src/types.ts
+++ src/types.ts
@@ -1,30 +1,30 @@
-export interface HarvesterChartProps  {
-  option: {}
-  handleClick: (data: {value: []}) => void
+export interface HarvesterChartProps {
+	option: {};
+	handleClick: (data: { value: [] }) => void;
 }
 
 export interface Parameter {
-  code: string
-  title?: string
+	code: string;
+	title?: string;
 }
 
 export interface StartEndTimeSpan {
-  start_time: string
-  end_time: string
-  time_step: number
+	start_time: string;
+	end_time: string;
+	time_step: number;
 }
 
 export interface GlobalStateProps {
-   startEndTimeSpan: StartEndTimeSpan
-   trafficabilityData: []
-   soilWetnessData: [] 
-   soilTemperatureData: []
-   snowHeight: []
-   checked: boolean,
-   parameters: {
-    trafficability: Parameter[]
-    snowHeight: Parameter[]
-    soilTemperature: Parameter[]
-    soilWetness: Parameter[]
-   }
- }
+	startEndTimeSpan: StartEndTimeSpan;
+	trafficabilityData: [];
+	soilWetnessData: [];
+	soilTemperatureData: [];
+	snowHeight: [];
+	checked: boolean;
+	parameters: {
+		trafficability: Parameter[];
+		snowHeight: Parameter[];
+		soilTemperature: Parameter[];
+		soilWetness: Parameter[];
+	};
+}
