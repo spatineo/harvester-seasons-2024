2aa7936c1743008b19289dea91b7a44e1828fcad
Stanley Festus <stanley.festus@spatineo.com>
2023-05-05 11:10:58 +0000

    lang slice eslint setting applied

--- src/Lang/languageSlice.ts
+++ src/Lang/languageSlice.ts
@@ -1,21 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 export interface LanguageOptions {
-  en: string
+	en: string;
 }
 
 const initialState: LanguageOptions = {
- en: 'en',
-}
+	en: 'en',
+};
 
 const languageSlice = createSlice({
-  name: 'language',
-  initialState,
-  reducers: {
-    changeLanguage: (state, action) => {
-      console.log(state, action.payload)
-    }
-  }
-})
+	name: 'language',
+	initialState,
+	reducers: {
+		changeLanguage: (state, action) => {
+			console.log(state, action.payload);
+		},
+	},
+});
 
-export default languageSlice.reducer
+export default languageSlice.reducer;
