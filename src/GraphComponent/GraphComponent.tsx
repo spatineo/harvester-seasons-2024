526ee72fcb2fa606fa5a93aadf83f613b1de2a72
Stanley Festus <stanley.festus@spatineo.com>
2023-05-12 09:50:20 +0000

    not merge prop added to control animation

--- src/GraphComponent/GraphComponent.tsx
+++ src/GraphComponent/GraphComponent.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable import/default */
 import React from 'react';
 import ReactECharts from 'echarts-for-react';
-import * as echarts from 'echarts';
 
 interface GraphProps {
 	onEvents: {};
@@ -9,7 +8,7 @@ interface GraphProps {
 }
 
 const GraphComponent: React.FC<GraphProps> = ({ onEvents, option }) => {
-	return <ReactECharts option={option} theme="" onEvents={onEvents} />;
+	return <ReactECharts option={option} theme="" onEvents={onEvents} notMerge={true} />;
 };
 
 export default GraphComponent;
