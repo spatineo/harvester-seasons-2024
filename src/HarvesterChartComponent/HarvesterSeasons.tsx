86e9e0ed4cb18a1d4c76105f40ef0af306754240
Stanley Festus <festusstanley@gmail.com>
2023-06-01 12:24:28 +0000

    chart values wip

--- src/HarvesterChartComponent/HarvesterSeasons.tsx
+++ src/HarvesterChartComponent/HarvesterSeasons.tsx
@@ -1,18 +1,33 @@
 // eslint-disable-next-line import/default
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Box } from '@mui/material';
 import GraphComponent from '../GraphComponent/GraphComponent';
+import EchartsComponent from '../EchartsComponent/EchartsComponent'
 import { HarvesterChartProps } from '../types';
 
 const HarvesterSeasons: FC<HarvesterChartProps> = ({
 	option,
+	showLegend,
 	handleClick,
 	handleOnmouseEnter,
 	handleOnmouseLeave,
 }) => {
+	const [selectedDate, setSelectedDate] = useState<string | null>(null);
+  const [selectedYValues, setSelectedYValues] = useState<(string | number)[]>([]);
+
+  const handleSetValuesProps = (date: string | null, yValues: (string | number)[]) => {
+    setSelectedDate(date);
+    setSelectedYValues(yValues);
+  };
+
+
+	console.log(selectedDate)
 	return (
 		<Box>
-			{option ? (
+			<Box>
+				Box
+			</Box>
+			{/* {option ? (
 				<GraphComponent
 					onEvents={{
 						click: handleClick,
@@ -23,7 +38,13 @@ const HarvesterSeasons: FC<HarvesterChartProps> = ({
 				/>
 			) : (
 				<span>No data available</span>
-			)}
+			)} */}
+			{
+				option ? (
+					<EchartsComponent option={option} setValuesProps={handleSetValuesProps}  />
+				) :
+				(<Box>No data</Box>)
+			}
 		</Box>
 	);
 };
