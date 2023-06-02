b416d240503853f867ebe3b95a941e1d60b7d856
Stanley Festus <festusstanley@gmail.com>
2023-06-02 07:44:22 +0000

    HarvesterChart props updated

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,7 @@
 export interface HarvesterChartProps {
 	option: {};
-	showLegend: boolean;
 	handleClick: (data: { value: [] }) => void;
-	handleOnmouseEnter: (data: { value: [] }) => void;
-	handleOnmouseLeave: (data: { value: [] }) => void;
+	handleOnmouseEnter: (data: [string | null, ...number[]]) => void;
 }
 
 export interface Parameter {
