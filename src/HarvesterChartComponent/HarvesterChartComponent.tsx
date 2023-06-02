0e37f35b351372e923e97e9d656211f93707a7b2
Stanley Festus <festusstanley@gmail.com>
2023-06-02 07:45:29 +0000

    EchartsComponent import

---
+++ src/HarvesterChartComponent/HarvesterChartComponent.tsx
@@ -0,0 +1,28 @@
+// eslint-disable-next-line import/default
+import React, { FC, useState } from 'react';
+import { Box } from '@mui/material';
+import EchartsComponent from '../EchartsComponent/EchartsComponent'
+import { HarvesterChartProps } from '../types';
+
+const HarvesterSeasons: FC<HarvesterChartProps> = ({
+	option,
+	handleClick,
+	handleOnmouseEnter,
+}) => {
+	
+	return (
+		<Box>
+			<Box>
+				Box
+			</Box>
+			{
+				option ? (
+					<EchartsComponent option={option} setValuesProps={handleOnmouseEnter} />
+				) :
+				(<Box>No data</Box>)
+			}
+		</Box>
+	);
+};
+
+export default HarvesterSeasons;
