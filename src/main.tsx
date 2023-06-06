6d8c1fcab54c5d34d2d11021756446d1f9a808ca
Stanley Festus <festusstanley@gmail.com>
2023-06-06 10:43:35 +0000

    added notistack snackbar provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,25 @@
 /* eslint-disable import/default */
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
-import App from './App';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { SnackbarProvider } from "notistack";
+import { SnackbarUtilsConfigurator } from './store/hooks'
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root') as HTMLElement).render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</React.StrictMode>
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <SnackbarProvider
+        maxSnack={4}
+        autoHideDuration={8000}
+        anchorOrigin={{ horizontal: "center", vertical: "bottom" }}
+      >
+				<SnackbarUtilsConfigurator />
+        <App />
+      </SnackbarProvider>
+    </Provider>
+  </React.StrictMode>
 );
