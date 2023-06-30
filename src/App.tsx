fdd5b5270dbfb2b9ec62c41bde7af2259f642889
Stanley Festus <festusstanley@gmail.com>
2023-06-30 07:46:19 +0000

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
+/* eslint-disable import/no-extraneous-dependencies */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, import/default
-import React from 'react';
-import { Box } from '@mui/material';
-import MainViewComponent from './MainView/MainViewComponent';
-import 'ol/ol.css';
-import 'ol-layerswitcher/dist/ol-layerswitcher.css';
-import './App.css';
+import React from "react";
+import { Box } from "@mui/material";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainViewComponent from "./MainView/MainViewComponent";
+import PrivacyPolicy from "./PrivacyPolicy/PrivacyPolicy";
+import "ol/ol.css";
+import "ol-layerswitcher/dist/ol-layerswitcher.css";
+import "./App.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (<MainViewComponent />),
+  },
+  {
+    path: "privacy-policy",
+    element: (<PrivacyPolicy />),
+  },
+]);
 
 function App() {
-	return (
-		<div className="App">
-			<Box>
-				<MainViewComponent />
-			</Box>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Box>
+				<RouterProvider router={router} />
+      </Box>
+    </div>
+  );
 }
 
 export default App;
