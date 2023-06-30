a80043fae7f844a73e6a07ff13f076d4a3109d70
Stanley Festus <festusstanley@gmail.com>
2023-06-30 08:19:23 +0000

    removed unused types

--- src/EchartsComponent/EchartsComponent.tsx
+++ src/EchartsComponent/EchartsComponent.tsx
@@ -6,26 +6,11 @@ import *  as echarts from 'echarts'
 import { Box } from '@mui/material'
 import {  EChartOption } from 'echarts'
 
-type EventParams = {
-  componentType: string,
-  seriesType: string,
-  seriesIndex: number,
-  seriesName: string,
-  name: string,
-  dataIndex: number,
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  data: Object,
-  dataType: string,
-  value: number | Array<any> | any,
-  color: string
-};
-
 interface ChartProps {
   option: EChartOption;
-  setValuesProps: (data: [string | null, ...number[]]) => void;
 }
 
-const EchartsCompoent: React.FC<ChartProps> = ({ option, setValuesProps }) => {
+const EchartsCompoent: React.FC<ChartProps> = ({ option }) => {
   const chartRef = useRef<HTMLDivElement>(null)
   const [chart, setChart] = useState<echarts.ECharts | null>(null)
 
@@ -48,41 +33,6 @@ const EchartsCompoent: React.FC<ChartProps> = ({ option, setValuesProps }) => {
     }
   }, [chart, option])
 
-  useEffect(() => {
-    if(!chart) {
-      return;
-    }
-    chart.on('mouseover', function(params: EventParams){
-      const date: any = params.value[0];
-      const yValues: number[] = [];
-
-  option.series?.forEach((series) => {
-    if (Array.isArray(series.data)) {
-      series.data.forEach((dataItem: any) => {
-        if (dataItem[0] === date) {
-          yValues.push(dataItem[1]);
-        }
-      });
-    }
-  });
-  setValuesProps([date, ...yValues])
-
-    })
-
-  }, [chart, setValuesProps])
-  
-useEffect(() => {
-  if(!chart) {
-    return;
-  }
-
-  chart.on('globalout', function() {
-    const data: any  = []
-    setValuesProps(data)
-  })
-
-}, [chart]
-)
     return (
     <Box ref={chartRef} style={{ width: '100%', height: '300px' }}></Box>
   )
