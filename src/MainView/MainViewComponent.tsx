730ea5a86d24921c5e6dada2ce0615ce591a317f
Stanley Festus <festusstanley@gmail.com>
2023-07-04 06:33:32 +0000

    moved away create trafficbility options function

--- src/MainView/MainViewComponent.tsx
+++ src/MainView/MainViewComponent.tsx
@@ -2,24 +2,24 @@
 /* eslint-disable import/default */
 import React, { useEffect, useState } from "react";
 import { Container, Box, ListItemButton, Collapse } from "@mui/material";
-import { EChartOption } from "echarts";
-import { useRootDispatch } from "../store/hooks";
+import { EChartOption } from 'echarts';
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
 import TraficabilityGraph from "../TrafficabilityGraph/Trafficability";
 import GraphView from "../GraphView/GraphView";
 import SwitchComponent from "../SwitchComponent/SwitchComponent";
 import * as constants from "../store/constants";
 import HarvesterMap from "../HarvesterMapComponent/HarvesterMap";
-import { useAppSelector } from "../store/hooks";
+import { useAppSelector, useRootDispatch } from "../store/hooks";
 import { RootState } from "../store/store";
-import { Parameter } from "../types";
-import { ExpandLess, ExpandMore } from "@mui/icons-material";
 import { LanguageOptions } from "../Lang/languageSlice";
 import { languages } from "../Lang/languages";
-import testimonial from '../assets/testimonial_metsateho1.png';
+import testimonial from "../assets/testimonial_metsateho1.png";
+import { createTrafficabilityGraphOptions } from "../utils";
+import { actions } from "../globalSlice";
 
 function MainViewComponent() {
-  const [trafficabilityGraphOtion, setTrafficabilityGraphOption] =
-    useState<any>(null);
+  const [trafficabilityGraphOption, setTrafficabilityGraphOption] =
+  useState<EChartOption | null>(null);
   const [summer, setSummer] = useState<string>("");
   const [winter, setWinter] = useState<string>("");
   const dispatch = useRootDispatch();
@@ -29,108 +29,9 @@ function MainViewComponent() {
   const graphParameters = useAppSelector(
     (state: RootState) => state.global.parameters
   );
-  const mark = useAppSelector((state: RootState) => state.global.markLine );
+  const mark = useAppSelector((state: RootState) => state.global.markLine);
   const [open, setOpen] = useState(false);
-	const information = useAppSelector((state) => state.language);
-
-  const createOptions = (parameters: Parameter[], values: []) => {
-    const trafficabilityOptionData: EChartOption = {
-      legend: {},
-      grid: {
-        height: "80px",
-      },
-      tooltip: {
-        trigger: "axis",
-      },
-      yAxis: {
-        name: "Traficability",
-        nameLocation: "middle",
-        nameTextStyle: {
-          padding: 18,
-        },
-      },
-      xAxis: {
-        type: "time",
-      },
-      series: [
-        {
-          label: {
-            show: false,
-          },
-          type: "line",
-          seriesLayoutBy: "row",
-          markLine: {
-            data: [
-              {
-                xAxis: mark,
-                name: "",
-                type: "min",
-                label: { show: false },
-              },
-            ],
-            symbol: 'none',
-            lineStyle: {
-            type: 'solid',
-            width: 3,
-            },
-          },
-        },
-        {
-          type: "line",
-          name: "winter",
-          areaStyle: {
-            color: "rgba(0, 12, 0, 0.3)",
-          },
-          data: [
-            ...values.map((t: { utctime: string; [key: string]: string }) => {
-              return [
-                new Date(t.utctime).toISOString(),
-                ...parameters.map((p) => {
-                  if (t[p.code] !== null && Number(t[p.code]) === 0) {
-                    return 0;
-                  } else if (
-                    Number(t[p.code]) > 0.0 &&
-                    Number(t[p.code]) <= 0.099999
-                  ) {
-                    return 1;
-                  } else {
-                    return 2;
-                  }
-                }),
-              ];
-            }),
-          ],
-        },
-        {
-          type: "line",
-          name: "summer",
-          areaStyle: {
-            color: "rgba(0, 128, 255, 0.3)",
-          },
-          data: [
-            ...values.map((t: { utctime: string; [key: string]: string }) => {
-              return [
-                new Date(t.utctime).toISOString(),
-                ...parameters.map((p) => {
-                  if (t[p.code] !== null && Number(t[p.code]) === 0) {
-                    return 0.5;
-                  } else if (
-                    Number(t[p.code]) > 0.0 &&
-                    Number(t[p.code]) <= 0.099999
-                  ) {
-                    return 1.5;
-                  } else {
-                    return 2;
-                  }
-                }),
-              ];
-            }),
-          ],
-        },
-      ],
-    };
-    return trafficabilityOptionData;
-  };
+  const information = useAppSelector((state) => state.language);
 
   useEffect(() => {
     dispatch({ type: constants.TRAFFICABILITY_API });
@@ -141,38 +42,49 @@ function MainViewComponent() {
 
   useEffect(() => {
     if (trafficability) {
-      const trafficabilityOption = createOptions(
+      const trafficabilityOption = createTrafficabilityGraphOptions(
         graphParameters.sixMonthParams.trafficability,
-        trafficability
+        trafficability,
+        mark
       );
       setTrafficabilityGraphOption(trafficabilityOption);
     }
   }, [trafficability, graphParameters.sixMonthParams.trafficability, mark]);
-  
-  const handleClick = () => {
-		setOpen(!open);
-	};
 
+  const handleClick = () => {
+    setOpen(!open);
+  };
+  console.log(mark, 'in traffica')
 
   return (
     <Box>
       <Container maxWidth="lg">
         <Box>
-					<ListItemButton onClick={handleClick} style={{ paddingLeft: '0px', width: '26%' }}>
-						{open ? <ExpandLess /> : <ExpandMore />}{' '}
-						{languages.info[information.en as keyof LanguageOptions]}
-					</ListItemButton>
-					<Collapse in={open} timeout="auto" unmountOnExit>
-						<Box sx={{ width: '80%', margin: '2rem auto' }}>
-							<Box component="img" src={testimonial} sx={{ width: '80%' }} />
-						</Box>
-						<Box>{languages.overviewBody[information.en as keyof LanguageOptions]}</Box>
-					</Collapse>
-				</Box>
+          <ListItemButton
+            onClick={handleClick}
+            style={{ paddingLeft: "0px", width: "26%" }}
+          >
+            {open ? <ExpandLess /> : <ExpandMore />}{" "}
+            {languages.info[information.en as keyof LanguageOptions]}
+          </ListItemButton>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ width: "80%", margin: "2rem auto" }}>
+              <Box component="img" src={testimonial} sx={{ width: "80%" }} />
+            </Box>
+            <Box>
+              {languages.overviewBody[information.en as keyof LanguageOptions]}
+            </Box>
+          </Collapse>
+        </Box>
         {trafficability.length > 0 ? (
-          <TraficabilityGraph option={trafficabilityGraphOtion} />
+          <TraficabilityGraph
+            option={trafficabilityGraphOption}
+            onGraphClick={function (xAxisData: string): void {
+              dispatch(actions.setMarkLine(xAxisData))
+            }}
+          />
         ) : (
-          <Box className="loading"> Loading  ....</Box>
+          <Box className="loading"> Loading ....</Box>
         )}
         <SwitchComponent />
         <HarvesterMap />
