7a7136213f5c3758a4b68ca51af9f6506e0e7e67
Stanley Festus <festusstanley@gmail.com>
2023-08-10 09:03:00 +0300

    added coverage json

--- src/LeafletMap/LeafletMap.tsx
+++ src/LeafletMap/LeafletMap.tsx
@@ -12,7 +12,11 @@ import * as CovJSON from "covjson-reader";
 import * as C from "leaflet-coverage";
 import "leaflet/dist/leaflet.css";
 
-const LeafletComponent: React.FC = () => {
+interface MapProps {
+  children: React.ReactNode;
+}
+
+const LeafletComponent: React.FC<MapProps> = ({ children}) => {
   const leafletRef = useRef();
   const [map, setMap] = useState<L.Map | null>(null);
 
@@ -20,34 +24,11 @@ const LeafletComponent: React.FC = () => {
     if (!leafletRef.current) {
       return;
     }
-    const mapObject: L.Map = L.map(leafletRef.current).setView([64.0, 26.0], 5);
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution: "© OpenStreetMap contributors",
-    }).addTo(mapObject);
+    const mapObject: L.Map = L.map(leafletRef.current).setView(
+      [66.497, 24.9],
+      5
+    );
 
-    const layers = L.control
-      .layers(undefined, undefined, { collapsed: false })
-      .addTo(mapObject);
-
-    let layer;
-    const dataUrl =
-      "https://raw.githubusercontent.com/covjson/cookbook/master/examples/coverages/grid.covjson";
-      CovJSON.read(dataUrl).then(function (coverage) {
-        layer = C.dataLayer(coverage, {parameter: 'temperature'})
-          .on('afterAdd', function () {
-            C.legend(layer).addTo(mapObject)
-            mapObject.fitBounds(layer.getBounds())
-          })
-          .addTo(mapObject)
-        layers.addOverlay(layer, 'Temperature')
-      })
-      
-      mapObject.on('click', function (e) {
-        new C.DraggableValuePopup({
-          layers: [layer]
-        }).setLatLng(e.latlng).openOn(mapObject)
-      })
-      
     setMap(mapObject);
 
     return () => {
@@ -55,12 +36,60 @@ const LeafletComponent: React.FC = () => {
     };
   }, []);
 
+    useEffect(() => {
+    if(!map){
+      return;
+    }
+
+    const baseLayers = {
+      "OpenTopoMap": L.tileLayer("https://a.tile.opentopomap.org/{z}/{x}/{y}.png", {
+        attribution: "© OpenStreetMap contributors",
+      }),
+      // Add other base layers if needed
+    };
+    const overlayLayers = {
+      "Temperature": L.marker([61.9241, 25.7482]),
+    };
+    const layers = L.control
+      .layers(baseLayers, overlayLayers, { collapsed: false })
+      .addTo(map);
+  
+    let layer;
+    const dataUrl =
+      "https://raw.githubusercontent.com/covjson/cookbook/master/examples/coverages/grid.covjson";
+    CovJSON.read(dataUrl).then(function (coverage) {
+      layer = C.dataLayer(coverage, { parameter: "temperature" })
+        .on("afterAdd", function () {
+          C.legend(layer).addTo(map);
+          map.fitBounds(layer.getBounds());
+        })
+        .addTo(map);
+        layers.addOverlay(layer, "Temperature");
+    });
+
+    map.on("click", function (e) {
+      new C.DraggableValuePopup({
+        layers: [layer],
+      })
+        .setLatLng(e.latlng)
+        .openOn(map);
+    });
+
+    return () => {
+      if(map){
+        map.removeLayer(layer);
+      }
+    }
+  }, [map])
+   
   return (
     <LeafletContext.Provider value={{ map }}>
       <Box
         ref={leafletRef}
-        style={{ width: "80%", height: "600px", margin: "auto" }}
-      ></Box>
+        style={{ width: "80%", height: "700px", margin: "auto" }}
+      >
+        {children}
+      </Box>
     </LeafletContext.Provider>
   );
 };
