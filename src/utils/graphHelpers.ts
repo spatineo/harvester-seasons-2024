30d1eb6ec6f251ae151c04d4dc0327bfd164b304
Stanley Festus <festusstanley@gmail.com>
2023-08-15 11:31:42 +0300

    helper functions to create graph seperated to own file

---
+++ src/utils/graphHelpers.ts
@@ -0,0 +1,205 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { Parameter, Smartmet, GraphOptions } from "../types";
+import { EChartOption } from "echarts";
+
+const monthNames = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+
+export function createTrafficabilityGraphOptions(
+  parameters: Parameter[],
+  values: [],
+  mark
+) {
+  const trafficabilityOptionData: EChartOption = {
+    legend: {},
+    grid: {
+      height: "80px"
+    },
+    tooltip: {
+      trigger: "axis"
+    },
+    yAxis: {
+      name: "Traficability",
+      nameLocation: "middle",
+      nameTextStyle: {
+        padding: 18
+      }
+    },
+    xAxis: {
+      type: "time",
+      axisLabel: {
+        formatter: (value: Date) => {
+          const date = new Date(value);
+          const month = monthNames[date.getMonth()];
+          const year = date.getFullYear();
+          return `${month} ${year}`;
+        }
+      }
+    },
+    series: [
+      {
+        label: {
+          show: false
+        },
+        type: "line",
+        seriesLayoutBy: "row",
+        markLine: {
+          data: [
+            {
+              xAxis: mark,
+              name: "",
+              type: "min",
+              label: { show: false }
+            }
+          ],
+          symbol: "none",
+          lineStyle: {
+            type: "solid",
+            width: 3
+          }
+        }
+      },
+      {
+        type: "line",
+        name: "Summer",
+        areaStyle: {
+          color: "rgba(0, 12, 0, 0.3)"
+        },
+        data: [
+          ...values.map((t: { utctime: string; [key: string]: string }) => {
+            return [
+              new Date(t.utctime).toISOString(),
+              ...parameters.map((p) => {
+                if (p.code === "HARVIDX{0.4;SWVL2-M3M3:SMARTMET:5015}") {
+                  if (Number(t[p.code]) === 2) {
+                    return "nan";
+                  }
+                } else {
+                  return 2;
+                }
+              })
+            ];
+          })
+        ]
+      },
+      {
+        type: "line",
+        name: "Winter",
+        areaStyle: {
+          color: "rgba(0, 128, 255, 0.3)"
+        },
+        data: [
+          ...values.map((t: { utctime: string; [key: string]: string }) => {
+            return [
+              new Date(t.utctime).toISOString(),
+              ...parameters.map((p) => {
+                if (p["ensover{0.4;0.9;HSNOW-M:SMARTOBS:13:4}"] !== null) {
+                  return Math.max(
+                    Number(t["ensover{0.4;0.9;HSNOW-M:SMARTOBS:13:4}"]),
+                    Number(
+                      t[
+                        "HARVIDX{273;TSOIL-K:ECBSF:::7:3:1-50;TSOIL-K:ECBSF:::7:1:0}"
+                      ]
+                    )
+                  );
+                } else {
+                  return "";
+                }
+              })
+            ];
+          })
+        ]
+      }
+    ]
+  };
+  return trafficabilityOptionData;
+}
+
+export function createOptions(
+  opts: GraphOptions,
+  parameters: Parameter[],
+  values: Smartmet[],
+  mark: string,
+  padding: [number, number, number, number]
+) {
+  return {
+    animation: false,
+    animationThreshold: 1,
+    grid: {},
+    tooltip: {
+      trigger: "item"
+    },
+    yAxis: {
+      name: opts.title,
+      nameLocation: "middle",
+      nameTextStyle: {
+        padding
+      }
+    },
+    xAxis: {
+      type: "time",
+      axisLabel: {
+        formatter: (value: Date) => {
+          const date = new Date(value);
+          const month = monthNames[date.getMonth()];
+          const year = date.getFullYear();
+          return `${month} ${year}`;
+        }
+      }
+    },
+    series: [
+      {
+        label: {
+          show: false
+        },
+        type: "line",
+        seriesLayoutBy: "row",
+        markLine: {
+          data: [
+            {
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+              xAxis: mark,
+              name: "",
+              type: "min",
+              label: { show: false }
+            }
+          ],
+          symbol: "none",
+          lineStyle: {
+            type: "solid",
+            width: 3,
+            arrow: "none"
+          }
+        }
+      },
+      ...parameters.map((p: { code: string }, i: number) => {
+        const codes = p.code;
+        return {
+          type: "line",
+          symbolSize: 2,
+          name: `SH-${i}`,
+          data: values.map(
+            (d: { utctime: string; [key: string]: string | number | null }) => {
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+              return [d.utctime, d[codes]];
+            }
+          )
+        };
+      })
+    ]
+  };
+}
