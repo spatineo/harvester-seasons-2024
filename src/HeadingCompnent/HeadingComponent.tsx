a692695a2cf90de5f3c1dbbf18d71015bb534de7
Stanley Festus <festusstanley@gmail.com>
2023-09-22 14:18:52 +0300

    adding information WIP

--- src/HeadingCompnent/HeadingComponent.tsx
+++ src/HeadingCompnent/HeadingComponent.tsx
@@ -13,16 +13,24 @@ import {
   FormControl,
   MenuItem,
   InputLabel,
+  Typography,
 } from "@mui/material";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { ArrowDropDown, ArrowRight } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import { useAppSelector, useRootDispatch } from "../store/hooks";
-import { LanguageOptions,actions } from "../Lang/languageSlice";
+import { LanguageOptions, actions } from "../Lang/languageSlice";
+import EmbeddedYouTubeVideo from "./EmbeddedYouTubeVideo";
+import FeedbackForm from "../FeedBackForm/FeedBack";
 import { languages } from "../Lang/languages";
 import logo from "../assets/logos_long.png";
 import smallScreen from "../assets/logos.png";
 import testimonial from "../assets/testimonial_metsateho1.png";
+import ohje2 from "../assets/ohje2.png";
+import ohje2a from "../assets/ohje2a.png";
+import ohje4 from "../assets/ohje4.png";
+import ohje5 from "../assets/ohje5b.png";
+import korjuukelpoisuus from "../assets/korjuukelpoisuus.png"
 
 const MobileLogo = styled("img")({
   "@media (max-width: 900px)": {
@@ -47,14 +55,19 @@ const BiggerScreen = styled("img")({
   },
 });
 
+const styles = {
+  font: {
+    fontFamily: "Lato",
+  },
+};
+
 const HeadingCompoment: React.FC = () => {
   const [open, setOpen] = useState(false);
   const information = useAppSelector((state) => state.language);
-  const dispatch = useRootDispatch()
-  
+  const dispatch = useRootDispatch();
 
   const handleSelectChange = (event: SelectChangeEvent) => {
-    dispatch(actions.changeLanguage(event.target.value))
+    dispatch(actions.changeLanguage(event.target.value));
   };
   const linkStyle = {
     textDecoration: "none",
@@ -63,8 +76,19 @@ const HeadingCompoment: React.FC = () => {
     setOpen(!open);
   };
 
+  const textWithAnchor =
+    languages.overviewBody[information.lang as keyof LanguageOptions];
+
   return (
-    <Grid container sx={{ width: "100%", position: "relative", top: "0.8rem", marginBottom: "2rem" }}>
+    <Grid
+      container
+      sx={{
+        width: "100%",
+        position: "relative",
+        top: "0.8rem",
+        marginBottom: "2rem",
+      }}
+    >
       <Grid container sx={{ width: "100%", padding: "0rem 1em" }}>
         <Grid item xs={6}>
           <Link to="/" style={linkStyle}>
@@ -86,8 +110,6 @@ const HeadingCompoment: React.FC = () => {
                   label="Age"
                   onChange={handleSelectChange}
                 >
-                  <MenuItem value="">
-                  </MenuItem>
                   <MenuItem value="en">en</MenuItem>
                   <MenuItem value="fi">fi</MenuItem>
                 </Select>
@@ -115,7 +137,136 @@ const HeadingCompoment: React.FC = () => {
             <Box component="img" src={testimonial} sx={{ width: "80%" }} />
           </Box>
           <Box sx={{ margin: "2rem auto" }}>
-            {languages.overviewBody[information.lang as keyof LanguageOptions]} hi
+            <Typography variant="h6" sx={styles.font}>
+              {
+                languages.overviewHeading[
+                  information.lang as keyof LanguageOptions
+                ]
+              }
+            </Typography>
+            <Typography sx={styles.font}>
+              <br />
+              <div dangerouslySetInnerHTML={{ __html: textWithAnchor }} />
+            </Typography>
+          </Box>
+          <EmbeddedYouTubeVideo />
+          <Box sx={{ margin: "1rem auto" }}>
+            <Typography variant="h6" sx={styles.font}>
+              {languages.howTo[information.lang as keyof LanguageOptions]}
+            </Typography>
+            {information.lang === "fi" ? (
+              <Box>
+                <ol>
+                  <li>
+                    {
+                      languages.instructions.one[
+                        information.lang as keyof LanguageOptions
+                      ]
+                    }
+                  </li>
+                  <br />
+                  <li>
+                    {
+                      languages.instructions.two[
+                        information.lang as keyof LanguageOptions
+                      ]
+                    }
+                    <Box
+                      component="img"
+                      src={ohje2}
+                      sx={{ marginTop: "0.4rem" }}
+                    ></Box>
+                    <ol type="a" style={{ margin: "0.6rem 0rem" }}>
+                      <li>
+                        <Box>
+                          <Box>
+                            {
+                              languages.mapTextOne[
+                                information.lang as keyof LanguageOptions
+                              ]
+                            }
+                          </Box>
+                          <br />
+                          <Box component="img" src={ohje2a}></Box>
+                        </Box>{" "}
+                        <br />
+                      </li>
+                      <li>
+                        <Box>
+                          {
+                            languages.mapTextTwo[
+                              information.lang as keyof LanguageOptions
+                            ]
+                          }
+                        </Box>
+                      </li>
+                    </ol>
+                  </li>
+                  <li>
+                    {
+                      languages.instructions.three[
+                        information.lang as keyof LanguageOptions
+                      ]
+                    }
+                  </li>
+                  <br />
+                  <li>
+                    {
+                      languages.instructions.four[
+                        information.lang as keyof LanguageOptions
+                      ]
+                    }
+                    <Box component="img" src={ohje4}></Box>
+                  </li>
+                  <br />
+                  <li>
+                    {
+                      languages.instructions.five[
+                        information.lang as keyof LanguageOptions
+                      ]
+                    }
+                     <ol type="a" style={{ margin: "0.6rem 0rem" }}>
+                      <li>
+                        <Box>
+                          {
+                            languages.mapTextInstructionFive1[
+                              information.lang as keyof LanguageOptions
+                            ]
+                          }
+                        </Box>
+                        <br />
+                      </li>
+                      <li>
+                        <Box>
+                          {
+                            languages.mapTextInstructionFive2[
+                              information.lang as keyof LanguageOptions
+                            ]
+                          }
+                        </Box>
+                      </li>
+                    </ol>
+                  </li>
+                </ol>
+                <Box component="img" src={ohje5}></Box><br/><br/>
+                <Box>Korjuukelpoisuusluokituksen muuttuminen säätiedon perusteella.</Box><br/>
+                <Box component="img" src={korjuukelpoisuus}></Box>
+                <FeedbackForm />
+              </Box>
+            ) : (
+              <Box>
+                {" "}
+                <Typography sx={styles.font}>
+                  <br />
+                  {
+                    languages.instructions.two[
+                      information.lang as keyof LanguageOptions
+                    ]
+                  }
+                </Typography>
+                <FeedbackForm />
+              </Box>
+            )}
           </Box>
         </Collapse>
       </Grid>
