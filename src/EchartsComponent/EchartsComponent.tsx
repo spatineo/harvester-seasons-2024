2cd881fa7ca02080c324c2ae7b79fd0e7765f0ee
Stanley Festus <festusstanley@gmail.com>
2023-10-04 09:10:16 +0300

    removed useState

--- src/EchartsComponent/EchartsComponent.tsx
+++ src/EchartsComponent/EchartsComponent.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect} from "react";
 import * as echarts from "echarts";
 import { Box } from "@mui/material";
 import { EChartOption } from "echarts";
