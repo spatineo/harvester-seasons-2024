e48eaa3b3804df06e55441b70f465fb1af32e7e0
Stanley Festus <festusstanley@gmail.com>
2023-10-05 09:23:51 +0300

    Added visibilty to map object

--- src/Layers/Maastokartta.tsx
+++ src/Layers/Maastokartta.tsx
@@ -11,10 +11,11 @@ import LayerTile from 'ol/layer/Tile';
 interface TileLayerProps {
 	source: TileSource;
 	title: string;
+	visible: boolean;
 }
 
 // eslint-disable-next-line react/prop-types
-const TileLayer: React.FC<TileLayerProps> = ({ source, title }) => {
+const TileLayer: React.FC<TileLayerProps> = ({ source, title, visible }) => {
 	const { map } = useContext(MapContext);
 
 	useEffect(() => {
@@ -23,6 +24,7 @@ const TileLayer: React.FC<TileLayerProps> = ({ source, title }) => {
 		const tileLayer = new LayerTile({
 			title,
 			type: 'base',
+			visible,
 			source,
 			className: 'class',
 		} as BaseLayerOptions);
