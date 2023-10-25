3ee2a27cf231a2b03f5c5d2a2e3faa036b19ef42
Stanley Festus <festusstanley@gmail.com>
2023-10-25 10:52:03 +0300

    WMSLayers added to initial state

--- src/MapComponent/MapComponentSlice.ts
+++ src/MapComponent/MapComponentSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { Map } from "../types";
+import { Map, WMSLayers } from "../types";
 
 const initialState: Map = {
   opacityValue: 70,
@@ -7,7 +7,8 @@ const initialState: Map = {
     lat: null,
     lon: null,
     resolution: 3550
-  }
+  },
+  WMSLayer: []
 };
 
 const mapComponentSlice = createSlice({
@@ -24,6 +25,9 @@ const mapComponentSlice = createSlice({
     setOpacity: (state, action: PayloadAction<number>) => {
       state.opacityValue = action.payload;
     },
+    setWMSLayer: (state, action: PayloadAction<WMSLayers[]>) => {
+      state.WMSLayer = action.payload
+    }
   }
 });
 
@@ -32,4 +36,5 @@ export default mapComponentSlice.reducer;
 export type ReduxActions =
   | ReturnType<typeof mapActions.setPosition>
   | ReturnType<typeof mapActions.setOpacity>
+  | ReturnType<typeof mapActions.setWMSLayer>
 
