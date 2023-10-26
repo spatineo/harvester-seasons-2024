8edaab44a048898db9fd4d2980a0d4a711bff2d6
Stanley Festus <festusstanley@gmail.com>
2023-10-26 09:05:54 +0300

    added layersToAdd where all layers are grouped

--- src/MapComponent/MapContext.ts
+++ src/MapComponent/MapContext.ts
@@ -1,9 +1,11 @@
 // eslint-disable-next-line import/default
 import { createContext } from 'react';
 import { Map } from 'ol';
+import LayerGroup from 'ol/layer/Group';
 
 interface MapContextType {
 	map: Map | null;
+	layersToAdd: LayerGroup
 }
 
 const MapContext = createContext({} as MapContextType);
