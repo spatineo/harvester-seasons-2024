94a9e0c076ad7d1fb2998ac68acf18dffbc890a5
Stanley Festus <festusstanley@gmail.com>
2023-10-30 09:10:53 +0200

    created new state with new values

--- src/MapComponent/MapComponentSlice.ts
+++ src/MapComponent/MapComponentSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { Map, WMSLayers } from "../types";
+import { Map, WMSLayerTimeStrategy } from "../types";
 
 const initialState: Map = {
   opacityValue: 70,
@@ -8,7 +8,33 @@ const initialState: Map = {
     lon: null,
     resolution: 3550
   },
-  WMSLayer: []
+
+  WMSLayerState: [
+    {
+      visible: false,
+      layerName: "gui:isobands:CERRA_FFG-MS",
+      opacity: 0.5,
+      WMSTimeStrategy: WMSLayerTimeStrategy.NoTimeDimesion
+    },
+    {
+      visible: false,
+      layerName: "gui:isobands:SWI_SWI2",
+      opacity: 0.5,
+      WMSTimeStrategy: WMSLayerTimeStrategy.NoTimeDimesion
+    },
+    {
+      visible: false,
+      layerName: "gui:isobands:ERA5L_TSOIL-K",
+      opacity: 0.5,
+      WMSTimeStrategy: WMSLayerTimeStrategy.NoTimeDimesion
+    },
+    {
+      visible: true,
+      layerName: "harvester:ecsf:HSNOW-M",
+      opacity: 0.5,
+      WMSTimeStrategy: WMSLayerTimeStrategy.NoTimeDimesion
+    }
+  ]
 };
 
 const mapComponentSlice = createSlice({
@@ -25,8 +51,16 @@ const mapComponentSlice = createSlice({
     setOpacity: (state, action: PayloadAction<number>) => {
       state.opacityValue = action.payload;
     },
-    setWMSLayer: (state, action: PayloadAction<WMSLayers[]>) => {
-      state.WMSLayer = action.payload
+    setWMSLayer: (state, action: PayloadAction<{visible: boolean, index: number}>) => {
+    
+      const newState = state.WMSLayerState.map((item, i) => {
+        if (i === action.payload.index) {
+          return { ...item, visible: action.payload.visible };
+        } else {
+          return { ...item, visible: false };
+        }
+      });
+      return { ...state, WMSLayerState: newState };
     }
   }
 });
@@ -36,5 +70,4 @@ export default mapComponentSlice.reducer;
 export type ReduxActions =
   | ReturnType<typeof mapActions.setPosition>
   | ReturnType<typeof mapActions.setOpacity>
-  | ReturnType<typeof mapActions.setWMSLayer>
-
+  | ReturnType<typeof mapActions.setWMSLayer>;
