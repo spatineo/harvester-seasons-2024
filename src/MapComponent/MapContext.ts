d024c4fc73d9b6fc68c7e0e81e9d0ba558659649
Stanley Festus <festusstanley@gmail.com>
2023-10-30 15:33:34 +0200

    layer name update

--- src/MapComponent/MapContext.ts
+++ src/MapComponent/MapContext.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/default
 import { createContext } from 'react';
 import { Map } from 'ol';
-import LayerGroup from 'ol/layer/Group';
+import LayerGroup from 'ol/layer/Group'; 
 
 interface MapContextType {
 	map: Map | null;
