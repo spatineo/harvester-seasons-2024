d3f3d2073064e72fc7a07456683be1b6b3218199
Stanley Festus <festusstanley@gmail.com>
2023-10-31 13:54:19 +0200

    Added legend conditionally

--- src/HarvesterMapComponent/HarvesterMap.tsx
+++ src/HarvesterMapComponent/HarvesterMap.tsx
@@ -4,10 +4,9 @@ import { Box } from "@mui/material";
 import MapComponent from "../MapComponent/MapComponent";
 import Layers from "../Layers/Layers";
 import Map from "../Layers/Map";
-import XYZ from "ol/source/XYZ";
 import LocationMarkerLayer from "../Layers/LocationMarker";
 import WMSLayer from "../Layers/WMSLayer";
-import { WMSLayerTimeStrategy, MapsStateProps } from "../types";
+import { WMSLayerTimeStrategy, MapsStateProps, WMSLayers } from "../types";
 import TrafficabilityTIFFLayer from "../Layers/TrafficabilityTIFFLayer";
 import { useAppSelector } from "../store/hooks";
 import { RootState } from "../store/store";
@@ -24,6 +23,7 @@ const HarvesterMap: React.FC = () => {
   );
   const { maps } = useAppSelector((state: RootState) => state.mapState);
   const [stateMap, setStateMap] = useState<MapsStateProps[]>([]);
+  const [wmLayer, setWMLayer] = useState<WMSLayers[]>([]);
 
   useEffect(() => {
     const parser = new WMSCapabilities();
@@ -45,9 +45,10 @@ const HarvesterMap: React.FC = () => {
   }, []);
 
   useEffect(() => {
-    if (!maps) return;
+    if (!maps || !WMSLayerState) return;
     setStateMap(maps);
-  }, [maps]);
+    setWMLayer(WMSLayerState);
+  }, [maps, WMSLayerState]);
 
   return (
     <>
@@ -69,8 +70,8 @@ const HarvesterMap: React.FC = () => {
                 );
               })}
 
-            {WMSLayerState.length > 0 &&
-              WMSLayerState.map((wms) => {
+            {wmLayer.length > 0 &&
+              wmLayer.map((wms) => {
                 return (
                   <Box key={wms.layerName}>
                     <WMSLayer
@@ -81,6 +82,21 @@ const HarvesterMap: React.FC = () => {
                       opacity={wms.opacity}
                       visible={wms.visible}
                     />
+                    {wms.legend.enabled === true && (
+                      <Box
+                        sx={{
+                          position: "absolute",
+                          zIndex: "100",
+                          height: "60%",
+                          bottom: "-0.6rem",
+                          left: "0.4rem",
+                          maxWidth: '24%',
+                          background: 'white'
+                        }}
+                        component="img"
+                        src={`https://desm.harvesterseasons.com/wms?REQUEST=GetLegendGraphic&VERSION=1.3.0&LAYER=${wms.layerName}&sld_version=1.1.0&style=&FORMAT=image/png&WIDTH=${wms.legend.width}&HEIGHT=${wms.legend.height}`}
+                      ></Box>
+                    )}
                   </Box>
                 );
               })}
@@ -96,11 +112,6 @@ const HarvesterMap: React.FC = () => {
               url="https://pta.data.lit.fmi.fi/geo/harvestability/KKL_SMK_Suomi_2021_06_01-UTM35.tif"
             />
             <LocationMarkerLayer title="Location Marker" />
-            <Box
-              sx={{ position: 'absolute', zIndex: '100', height: '60%', bottom: '0rem'}}
-              component="img"
-              src="https://desm.harvesterseasons.com/wms?REQUEST=GetLegendGraphic&VERSION=1.3.0&LAYER=gui:isobands:SWI_SWI2&sld_version=1.1.0&style=&FORMAT=image/png&WIDTH=65&HEIGHT=345"
-            ></Box>
           </Layers>
         </MapComponent>
       </Box>
