84aef1fe03a06a1f8a22182d5786b90762788cf4
stanleyfestus <festusstanley@gmail.com>
2024-01-05 09:12:54 +0200

    function name change

--- src/store/saga/config.saga.ts
+++ src/store/saga/config.saga.ts
@@ -1,7 +1,7 @@
 interface DataItem {
-  utctime: string;
   [key: string]: number | string;
 }
+
 export const filterFirstDayOfMonth = (data: DataItem[]) => {
   return data.filter((item, index) => {
     if (index === 0) {
@@ -9,7 +9,20 @@ export const filterFirstDayOfMonth = (data: DataItem[]) => {
     } else {
       const currentDate = new Date(item.utctime);
       const prevDate = new Date(data[index - 1].utctime);
-      return currentDate.getMonth() !== prevDate.getMonth() || currentDate.getDate() === 1;
+      return (
+        currentDate.getMonth() !== prevDate.getMonth() ||
+        currentDate.getDate() === 1
+      );
     }
   });
 };
+
+export function createGraphData(arr: DataItem[]) {
+  return arr.map((item) => {
+    const props: string[] = Object.keys(item).filter(
+      (key) => key !== "utctime"
+    );
+    const values = props.map((key) => (item[key] === null ? "nan" : item[key]));
+    return [item.utctime, ...values];
+  });
+}
