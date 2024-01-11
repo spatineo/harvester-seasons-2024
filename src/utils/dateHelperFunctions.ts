8ace94e07a1a2fff6d6a52612069e96d920a81e0
stanleyfestus <festusstanley@gmail.com>
2024-01-11 18:10:27 +0200

    refactor start end date wip

--- src/utils/dateHelperFunctions.ts
+++ src/utils/dateHelperFunctions.ts
@@ -12,16 +12,20 @@ export function tenYearsForward(date: Date, years: number) {
   return tenYears;
 }
 
-export function tenYearsBack(date: Date): Date {
-  const tenYearsAgo = new Date(date.getFullYear() - 10, 0, 1);
-    return tenYearsAgo;
+export function tenYearsBack(dateString: string): Date {
+  const newDate = new Date(dateString);
+  newDate.setUTCFullYear(newDate.getUTCFullYear() - 10);
+  newDate.setUTCMonth(0);
+  newDate.setUTCDate(1);
+  newDate.setUTCHours(0, 0, 0, 0);
+  return newDate;
 }
 
 export function lastDayOfPreviousYear(): Date {
   const today = new Date();
   const lastDay = new Date(today.getFullYear() - 1, 11, 31);
   return lastDay;
-} 
+}
 
 export function oneYearBackward(date: Date): Date {
   const tenYearsAgo = new Date(date.getFullYear(), 0, 1);
@@ -36,6 +40,15 @@ export function oneYearForward(date: Date) {
 export function addMonths(date: Date, months: number): Date {
   const newDate = new Date(date);
   newDate.setUTCMonth(date.getUTCMonth() + months, 0);
+  newDate.setUTCHours(0, 0, 0, 0);
+  return newDate;
+}
+
+export function reduceMonths(date: Date, months: number): Date {
+  const newDate = new Date(date);
+  newDate.setUTCMonth(date.getUTCMonth() - months, 0);
+  newDate.setUTCDate(1)
+  newDate.setUTCHours(0, 0, 0, 0);
   return newDate;
 }
 
