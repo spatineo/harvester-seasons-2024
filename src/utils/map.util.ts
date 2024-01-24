ee6c18d484916b5e85dad18bf654b9f2945033ce
stanleyfestus <festusstanley@gmail.com>
2024-01-24 15:58:21 +0200

    helper functions

--- src/utils/map.util.ts
+++ src/utils/map.util.ts
@@ -1,5 +1,51 @@
-import { WMSCapabilitiesLayerType, Smartmet, Parameter } from "../types";
+import {
+  WMSCapabilitiesLayerType,
+  Smartmet,
+  Parameter,
+  WMSLayerTimeStrategy
+} from "../types";
 import { getKeyFromFoundMatch } from "../globalSlice";
+import { RootState } from "../store/store";
+
+interface LayersWithLayerInfo {
+  layerName: string | undefined;
+  disabled: boolean;
+  layerInfo: null | WMSCapabilitiesLayerType;
+  utctime: string;
+  WMSTimeStrategy: WMSLayerTimeStrategy;
+  id: number;
+  visible: boolean;
+}
+
+interface FindOneDataFromParamsData {
+  utctime: string;
+}
+
+export const getMarkLineMatch = (param: string) => (state: RootState) => {
+  const rootState = state.global[param] as Smartmet[] | undefined;
+  const markLine = new Date(state.global.markLine);
+  if (!markLine) {
+    window.console.error(`check if you gave the expected string = ${param}`);
+    return;
+  }
+  if (rootState === undefined || rootState.length === 0) {
+    return `${param} array is empty`;
+  }
+  const foundMatch = rootState.find(
+    (item: { utctime: string | number | Date }) => {
+      const paramDate = new Date(item.utctime);
+
+      return (
+        paramDate.toISOString().split("T")[0] ===
+        markLine.toISOString().split("T")[0]
+      );
+    }
+  );
+  if (!foundMatch) {
+    return `No match found for markLine`;
+  }
+  return foundMatch;
+};
 
 export const checkForDataStringOrObject = (value: Smartmet | string) => {
   if (typeof value === "object") {
@@ -10,15 +56,7 @@ export const checkForDataStringOrObject = (value: Smartmet | string) => {
 };
 
 export const getLayersWithLayerInfo = (
-  array1: (
-    | {
-        layerName: string | undefined;
-        disabled: boolean;
-        layerInfo: null;
-        utctime: string;
-      }
-    | undefined
-  )[],
+  array1: (LayersWithLayerInfo | undefined)[],
   mapStateArray: WMSCapabilitiesLayerType[]
 ) => {
   return array1.map((res) => {
@@ -30,13 +68,18 @@ export const getLayersWithLayerInfo = (
       if (matchingLayer) {
         return {
           ...res,
-          layerName: matchingLayer.layerName,
-          layerInfo: matchingLayer
+          layerName: layerCode,
+          layerInfo: matchingLayer,
+          WMSTimeStrategy: WMSLayerTimeStrategy.ForceSelectedDate,
+          visible: true
         };
       } else {
         return {
           ...res,
-          disabled: true
+          disabled: true,
+          WMSTimeStrategy: WMSLayerTimeStrategy.ForceSelectedDate,
+          layerName: layerCode,
+          visible: false
         };
       }
     }
@@ -44,26 +87,33 @@ export const getLayersWithLayerInfo = (
 };
 
 export const getOneParamFromData = (
-  arr1: (string | { utctime: string } | null | undefined)[]
+  arr1: (FindOneDataFromParamsData | string | null | undefined)[]
 ) => {
-  return arr1.map((m) => {
+  return arr1.map((m, index) => {
     if (typeof m === "string") {
-      window.console.log(m);
+      window.console.error(m);
     }
     if (m !== null && typeof m === "object") {
       const keys = Object.keys(m);
       const filteredKeys = keys.filter((key) => key !== "utctime");
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
       return filteredKeys.length > 0 && m[filteredKeys[0]] !== null
         ? {
             ...m,
             disabled: false,
-            layerInfo: null
+            layerInfo: null,
+            layerName: "",
+            WMSTimeStrategy: WMSLayerTimeStrategy.ForceSelectedDate,
+            id: index,
+            visible: true
           }
         : {
             ...m,
             disabled: true,
-            layerInfo: null
+            layerInfo: null,
+            layerName: "",
+            WMSTimeStrategy: WMSLayerTimeStrategy.ForceSelectedDate,
+            id: index,
+            visible: false
           };
     }
   });
@@ -71,7 +121,7 @@ export const getOneParamFromData = (
 
 export const findMatchingLayers = (
   dataFromParams: (
-    | { disabled: boolean; layerInfo: null; utctime: string }
+    LayersWithLayerInfo
     | undefined
   )[],
   layersArray: Parameter[]
@@ -86,12 +136,14 @@ export const findMatchingLayers = (
       if (matchingLayer) {
         return {
           ...obj,
-          layerName: matchingLayer.layerName
+          layerName: matchingLayer.layerName,
+          
         };
       } else {
         return {
           ...obj,
-          layerName: ""
+          layerName: "",
+          
         };
       }
     }
