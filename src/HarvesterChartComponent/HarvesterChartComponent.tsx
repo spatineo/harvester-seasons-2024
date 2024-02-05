96f38f4f0a4b6564cc22d5ae69e4065ff0e40f87
stanleyfestus <festusstanley@gmail.com>
2024-02-05 13:03:44 +0200

    Implementation to handle tooltip formatting

--- src/HarvesterChartComponent/HarvesterChartComponent.tsx
+++ src/HarvesterChartComponent/HarvesterChartComponent.tsx
@@ -3,17 +3,29 @@ import { Box } from "@mui/material";
 import EchartsComponent from "../EchartsComponent/EchartsComponent";
 
 interface HarvesterChartProps {
-  option: {},
-  height: number
+  option: {};
+  height: number;
+  handleOnmouseEnter: (data: [string | null, ...number[]]) => void;
+  setMouseOver: (param: Record<string, string | number>[] | null) => void;
+  setMouseOut: (param: [] | null) => void;
 }
 const HarvesterSeasons: React.FC<HarvesterChartProps> = ({
   option,
   height,
+  handleOnmouseEnter,
+  setMouseOver,
+  setMouseOut,
 }) => {
   return (
     <Box>
       {option ? (
-        <EchartsComponent option={option} height={height} />
+        <EchartsComponent
+          option={option}
+          height={height}
+          setValuesProps={handleOnmouseEnter}
+          setMouseOut={setMouseOut}
+          setMouseOver={setMouseOver}
+        />
       ) : (
         <Box>No data</Box>
       )}
