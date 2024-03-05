16d5f8fbb2deb0e46dcfdcbb3bf34b7f99155628
stanleyfestus <festusstanley@gmail.com>
2024-03-05 08:59:24 +0200

    Implemented default position

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 /* eslint-disable import/no-extraneous-dependencies */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, import/default
-import React, { useEffect} from "react";
-import { Routes, Route } from "react-router-dom";
+import { useEffect} from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import MainViewComponent from "./MainView/MainViewComponent";
 import PrivacyPolicy from "./PrivacyPolicy/PrivacyPolicy";
 import HarvesterDashBoard from "./HarvesterDashBoard/HarvesterDashBoard";
 import * as constants from "./store/constants";
-import { useRootDispatch } from "./store/hooks";
+import { useRootDispatch, useAppSelector } from "./store/hooks";
+import { RootState } from "./store/store";
 import "ol/ol.css";
 import "ol-layerswitcher/dist/ol-layerswitcher.css";
 import "./App.css";
 
 function App() {
+  const { position } = useAppSelector((state: RootState) => state.mapState)
   const dispatch = useRootDispatch();
+  const navigate = useNavigate();
  
   useEffect(() => {
     dispatch({ type: constants.TRAFFICABILITY_API });
@@ -21,12 +24,17 @@ function App() {
     dispatch({ type: constants.SNOWHEIGHT_API });
     dispatch({ type: constants.WINDGUST_API });
     dispatch({ type: constants.SETWMSLAYERINFORMATION });
-  }, []);
+    if(position.lat !== null && position.lon !== null){
+    navigate(`/${position.lat},${position.lon}`)
+    }
+  }, [position.lat, position.lon]);
+
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<HarvesterDashBoard />}>
           <Route path="/" element={<MainViewComponent />} />
+          <Route path="/:position" element={<MainViewComponent />} />
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         </Route>
       </Routes>
